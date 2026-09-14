bb43e4e53fada9a4ca92ed98999cfa9b866eab71
Davi_dev1 <davigancalveesdasilva@gmail.com>
2026-09-14 09:25:44 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,18 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Teste Soma");
+/******************************************************************************
 
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
 
-int a, b;
-int x;
-
-Console.WriteLine("Digite  o valor de a = ");
-a = int .Parse(Console.ReadLine());
-
-Console.WriteLine("Digite  o valor de b = ");
-b = int.Parse(Console.ReadLine());
-
-
-
-x = a += b;
-
-Console.WriteLine("\nX = " + x);
+*******************************************************************************/
+using System;
+class HelloWorld {
+  static void Main() {
+      
+      int primeiroValor;
+      int segundoValor;
+      int produtoDosValores;
+      
+    Console.WriteLine(" -------- Produtos Simples ------- ");
+    
+    
+    Console.WriteLine("Digite o primeiro valor:  ");
+    primeiroValor = int.Parse(Console.ReadLine());
+    // Console.WriteLine("O valor informado é : "+primeiroValor);
+    
+    
+    Console.WriteLine("Digite o segundo valor:  ");
+    segundoValor = int.Parse(Console.ReadLine());
+    // Console.WriteLine("O valor informado é : "+segundoValor);
+    
+    produtoDosValores = primeiroValor * segundoValor;
+    
+    
+    Console.WriteLine("PROD =  "+produtoDosValores);
+    
+    
+    
+  }
+}
