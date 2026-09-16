e1133470ce40070f5f2de476a222b40e6c42d2d1
Davi_dev1 <davigancalveesdasilva@gmail.com>
2026-09-16 11:10:41 -0300

    Add files via upload

---
+++ Atividade7.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int a, b, c, d;
+
+
+
+Console.WriteLine("Digite o primeiro valor : ");
+a = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o segundo valor : ");
+b = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o terceiro valor ");
+c = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o quarto valor");
+d = int.Parse(Console.ReadLine());
+
+
+int diferenca = (a * b - c * d);
+
+Console.WriteLine("DIFERENCA = " + diferenca);  
