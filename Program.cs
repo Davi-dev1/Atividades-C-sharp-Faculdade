26d2c394445dd4a56a0e9bf94f98e8f4ba57b7db
Davi_dev1 <davigancalveesdasilva@gmail.com>
2026-09-16 09:45:51 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Teste Soma");
+
+
+int a, b;
+int x;
+
+Console.WriteLine("Digite  o valor de a = ");
+a = int .Parse(Console.ReadLine());
+
+Console.WriteLine("Digite  o valor de b = ");
+b = int.Parse(Console.ReadLine());
+
+
+
+x = a += b;
+
+Console.WriteLine("\nX = " + x);
